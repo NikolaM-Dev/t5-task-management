2853f41b1df982bb2a8f11ddea67fea09b91b833
Juan David Merchan Torres <juan1014166@gmail.com>
2026-06-19 13:43:07 -0500

    feat: remove unused code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import { getData } from '#/data/get-neon-data';
-
 export const Route = createFileRoute('/')({
   component: Home,
-
-  loader: async () => {
-    return getData();
-  },
 });
 
 function Home() {
-  const data = Route.useLoaderData();
-
-  return <>{data}</>;
+  return <>Hello World!</>;
 }
